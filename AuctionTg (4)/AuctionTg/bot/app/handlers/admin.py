93c8eb1dcef8c5e5446d1cec7c775e2732f93245
arsen00531 <arsendag505@gmail.com>
2024-06-30 23:05:30 +0300

    fix

--- AuctionTg (4)/AuctionTg/bot/app/handlers/admin.py
+++ AuctionTg (4)/AuctionTg/bot/app/handlers/admin.py
@@ -114,7 +114,12 @@ async def volume_set_(message: Message, state: FSMContext):
     try:
         if User.is_user_admin(message.from_user.id):
             volume = re.findall(r'[0-9]{1,}', message.text)
-            volume = float(f'{volume[0]}.{volume[1]}') if len(volume) > 0 else 0
+            if len(volume) == 1:
+                volume = float(f'{volume[0]}')
+            elif len(volume) == 2:
+                volume = float(f'{volume[0]}.{volume[1]}')
+            else:
+                volume = 0
             await state.update_data(volume=volume)
             await message.answer(
                 text=msg.abv_set_msg(message.from_user.id),
@@ -129,7 +134,12 @@ async def abv_set_(message: Message, state: FSMContext):
     try:
         if User.is_user_admin(message.from_user.id):
             volume = re.findall(r'[0-9]{1,}', message.text)
-            volume = float(f'{volume[0]}.{volume[1]}') if len(volume) > 0 else 0
+            if len(volume) == 1:
+                volume = float(f'{volume[0]}')
+            elif len(volume) == 2:
+                volume = float(f'{volume[0]}.{volume[1]}')
+            else:
+                volume = 0
             await state.update_data(abv=volume)
             await message.answer(
                 text=msg.country_set_msg(message.from_user.id),
